788fd284a6f466917bc70108b63f877830228114
fansac <fanzqc@gmail.com>
2020-07-17 05:21:34 +0100

    sigma need change

--- pano2flat.py
+++ pano2flat.py
@@ -10,78 +10,86 @@ https://stackoverflow.com/questions/29678510/convert-21-equirectangular-panorama
 
 class Pano:
     __epsion = 10e-7
-    def __init__(self, diretory, f = 1., fov = 0):
+    def __init__(self, diretory, fov = 90, viewpoint = (0., 0.), f = 1.):
         assert(f > 0.1 and f < 10.)
-        assert(fov >= 0 and fov < 180)
+        assert(fov > 0 and fov < 180)
+
         self.diretory = diretory
-        self.f = f
         self.fov = fov
+        self.theta = viewpoint[0] * np.pi / 180.
+        self.sigma = viewpoint[1] * np.pi / 180.
+        self.f = f
 
         # input image checking
+        #with cv2.imread(self.diretory) as img:
         img = cv2.imread(self.diretory)
         self.img = img.copy()
-        self.height, self.width, _ = self.img.shape 
-        assert(abs(self.width - 2 * self.height - 1) <= 2) 
+        self.height, self.width, self.channel = self.img.shape 
+        assert(abs(self.width - 2 * self.height - 1) <= 2)
+        assert(self.channel == 3)
+        
         
         #output image
-        self.sizeOriginalImage()
+        self.sizeOriginalOutputImage()
 
-    def sizeOriginalImage(self):
-        self.size = int(self.width / np.pi)
-        if self.fov != 0:
-            self.size *= np.tan(0.5 * self.fov)
-            
+    def sizeOriginalOutputImage(self):
+        self.r = self.height / np.pi 
+        self.size = self.size = 2. * self.r
+        self.size = 2.*self.r * np.tan(self.fov * np.pi / 360.)
+        self.r += self.__epsion
+        self.size = int(self.size)
     
     def show(self):
         print("input image shape", self.img.shape) 
         print("output image size {size}".format(size = self.size))
-        print(self.img[0:10, 0:10])
+        print(self.img)
         cv2.imshow('look', self.img)
         cv2.waitKey(0)
 
+    
+    def bilinearInterp(self, x, y, a):
+        assert(x >= 0 and x < a.shape[1] and y >= 0 and y < a.shape[0])
+        x0 = int(x) % a.shape[1]
+        y0 = int(y) % a.shape[0]
+        x1 = (x0 + 1) % a.shape[1]       # coords of pixel to bottom right
+        y1 = (y0 + 1) % a.shape[0]
+        nx = x - x0      # fraction of way across pixel
+        ny = y - y0
+        
+        # Pixel values of four corners
+        t_l = a[y0, x0]
+        t_r = a[y0, x1]
+        b_l = a[y1, x0]
+        b_r = a[y1, x1]
+        
+        # interpolate
+        res = (t_l*(1-ny)*(1-nx) + t_r*(1-ny)* nx + b_l*ny*(1-nx) + b_r*ny*nx).astype(np.uint8)
+        return res
+
     def pano2flat(self):
-        output_image = np.empty([self.size, self.size, 3], dtype = np.uint8) 
+        o_img = np.empty([self.size, self.size, self.channel], dtype = np.uint8) 
         ''' 
         x,y,z coordinate 
         x = 0.5* size
         y = 0.5 * size - j
         z = 0.5 * size - i
-
         '''
         x = 1.
-        r = 0.5 * self.size + self.__epsion
-        if r == 0:
-            print("error zero")
+        scale = np.tan(self.fov * np.pi / 360.)
         for i in range(self.size):
-            z = 1 - i / r
+            z = scale - i / self.r
             for j in range(self.size):
-                try:
-                    y = 1 - j / r
-                except RuntimeWarning:
-                    print("zero error:", i, j)
+                y = scale - j / self.r
                 # spherial coordinate
-                theta = np.mod(np.arctan2(y, x), 2*np.pi)
-                sigma = np.arccos(z/np.sqrt(x*x+y*y+z*z)) 
+                theta = np.mod(np.arctan2(y, x) - self.theta, 2*np.pi) 
+                sigma = np.mod(np.arccos(z/np.sqrt(x*x+y*y+z*z)) - self.sigma, 2 * np.pi) 
                 # local coordinate at input image
                 u = theta * self.width / (2. * np.pi)
                 v = sigma * self.height / np.pi
-                    
+                if (i > 500 and i < 505 and j > 300 and j < 320):
+                    print((theta, sigma))
                 # Use bilinear interpolation between the four surrounding pixels
-                ui = int(np.floor(u))  # coord of pixel to bottom left
-                vi = int(np.floor(v))
-                
-                u2 = ui+1       # coords of pixel to top right
-                v2 = vi+1
-                nu = u-ui      # fraction of way across pixel
-                nv = v-vi
-                
-                # Pixel values of four corners
-                A = self.img[vi % self.height,ui % self.width]
-                B = self.img[v2 % self.height,ui % self.width]
-                C = self.img[vi % self.height,u2 % self.width]
-                D = self.img[v2 % self.height,u2 % self.width]
-              
-                # interpolate
-                output_image[i,j]  = (A*(1-nv)*(1-nu) + B*(nv)*(1-nu) + C*(1-nv)*nu+D*nv*nu).astype(np.uint8)
+                o_img[i,j] = self.bilinearInterp(u, v, self.img)
 
-        return output_image
+        return o_img
+
